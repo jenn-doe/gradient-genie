818c87b1269afc74376a8549f21a90510e70df95
Jennifer MacFarlane <jenn-doe@users.noreply.github.com>
2020-01-11 23:47:34 -0800

    Updates

---
+++ precache-manifest.a9d7fa916cdf45fdbb0378f0bb5b203a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c12d0c3778959442149b9bd34a7c3c",
+    "url": "/gradient-genie/index.html"
+  },
+  {
+    "revision": "6eda0dae207a016d439b",
+    "url": "/gradient-genie/static/js/2.27409e30.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/gradient-genie/static/js/2.27409e30.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f5b93d4deb6f6d2394c",
+    "url": "/gradient-genie/static/js/main.8ab19655.chunk.js"
+  },
+  {
+    "revision": "665299744b3e3c996a0a",
+    "url": "/gradient-genie/static/js/runtime-main.6bfe1503.js"
+  }
+]);
